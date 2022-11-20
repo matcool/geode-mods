c80faee5f6f092b7634d605edcf3577f33f0ac7f
matcool <26722564+matcool@users.noreply.github.com>
2022-11-19 22:22:05 -0300

    fix merge.sh

--- merge.sh
+++ merge.sh
@@ -1,7 +1,7 @@
 for mod_name in */; do
 	if [ -f "$mod_name/mod.json" ]; then
 		mod_name=${mod_name:0:-1}
-		mod_id=$(grep -Po "\"id\": \".+?\"," $mod_name/mod.json | cut -d "\"" -f 4)
+		mod_id=$(grep -Eo "\"id\": \".+?\"," $mod_name/mod.json | cut -d "\"" -f 4)
 		echo "$mod_name - $mod_id"
 		args=""
 		if [ -f "built/$mod_name/Release/$mod_name.dll" ]; then
@@ -12,6 +12,6 @@ for mod_name in */; do
 			mv "built/$mod_name/lib$mod_name.dylib" "built/$mod_name/$mod_id.dylib"
 			args="$args --binary built/$mod_name/$mod_id.dylib"
 		fi
-		./geode.exe package new . $args --output $mod_name.geode
+		./geode.exe package new $mod_name $args --output $mod_name.geode
 	fi
 done
