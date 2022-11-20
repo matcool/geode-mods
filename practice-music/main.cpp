4d855334a8c5e52205b34f424476912c0cc79ec1
matcool <26722564+matcool@users.noreply.github.com>
2022-11-20 17:44:14 -0300

    add practice music

---
+++ practice-music/main.cpp
@@ -0,0 +1,42 @@
+#include <Geode/Geode.hpp>
+#include <Geode/modify/PlayLayer.hpp>
+#include <Geode/modify/GameSoundManager.hpp>
+
+USE_GEODE_NAMESPACE();
+
+bool lock_music = false;
+
+class $modify(PlayLayer) {
+	void startMusic() {
+		bool is_practice = m_isPracticeMode;
+		m_isPracticeMode = false;
+		PlayLayer::startMusic();
+		m_isPracticeMode = is_practice;
+	}
+
+	void destroyPlayer(PlayerObject* player, GameObject* obj) {
+		if (m_isPracticeMode && obj != m_antiCheatObject) {
+			GameSoundManager::sharedManager()->stopBackgroundMusic();
+		}
+		PlayLayer::destroyPlayer(player, obj);
+	}
+
+	void togglePracticeMode(bool toggle) {
+		if (!m_isPracticeMode && toggle) {
+			lock_music = true;
+		}
+		PlayLayer::togglePracticeMode(toggle);
+		if (lock_music) {
+			lock_music = false;
+			this->startMusic();
+		}
+	}
+};
+
+class $modify(GameSoundManager) {
+	void playBackgroundMusic(std::string name, bool a, bool b) {
+		if (!lock_music) {
+			GameSoundManager::playBackgroundMusic(name, a, b);
+		}
+	}
+};
