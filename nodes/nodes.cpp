10c1e565565ed9bc67971b5e64bbfa3a53c773bd
matcool <26722564+matcool@users.noreply.github.com>
2022-11-27 19:53:52 -0300

    fix macos not supporting <charconv> properly

--- nodes/nodes.cpp
+++ nodes/nodes.cpp
@@ -1,6 +1,7 @@
 #include "nodes.hpp"
 #include <array>
 #include <charconv>
+#include <fmt/core.h>
 
 using namespace cocos2d;
 
@@ -70,6 +71,15 @@ void FloatInputNode::textChanged(CCTextInputNode*) {
 void FloatInputNode::set_value(float value) {
 	// TODO: not hardcode this
 	std::array<char, 10> arr;
+#ifdef GEODE_IS_MACOS
+	// macos std::to_chars does not support floats at all, awesome
+	const auto str = fmt::format("{}", value);
+	if (str.size() >= 10) {
+		input_node->setString("1");
+	} else {
+		input_node->setString(str);
+	}
+#else
 	const auto result = std::to_chars(arr.data(), arr.data() + arr.size(), value, std::chars_format::fixed);
 	if (result.ec == std::errc::value_too_large) {
 		// TODO: have this be adjustable
@@ -78,13 +88,24 @@ void FloatInputNode::set_value(float value) {
 		*result.ptr = 0;
 		input_node->setString(arr.data());
 	}
+#endif
 }
 
 std::optional<float> FloatInputNode::get_value() {
 	const auto str = input_node->getString();
+#ifdef GEODE_IS_MACOS
+	try {
+		return std::stof(str);
+	} catch (const std::invalid_argument&) {
+		return {};
+	} catch (const std::out_of_range&) {
+		return {};
+	}
+#else
 	float value = 0.f;
 	const auto result = std::from_chars(str, str + std::strlen(str), value);
 	if (result.ec == std::errc::invalid_argument)
 		return {};
 	return value;
+#endif
 }
