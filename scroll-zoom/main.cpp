02ee5b250f7a686eee63a552ce1a7292d5f9824a
matcool <26722564+matcool@users.noreply.github.com>
2023-09-07 08:20:32 -0300

    add about.md to every mod

--- scroll-zoom/main.cpp
+++ scroll-zoom/main.cpp
@@ -8,11 +8,11 @@ class $modify(EditorUI) {
 	void scrollWheel(float y, float x) {
 		auto kb = CCDirector::sharedDirector()->getKeyboardDispatcher();
 		if (kb->getControlKeyPressed()) {
-			auto zoom = this->m_editorLayer->m_objectLayer->getScale();
+			auto zoom = m_editorLayer->m_objectLayer->getScale();
 			zoom = std::pow(std::numbers::e, std::log(std::max(zoom, 0.001f)) - y * 0.015f);
 			this->updateZoom(zoom);
 		} else {
-			auto layer = this->m_editorLayer->m_objectLayer;
+			auto layer = m_editorLayer->m_objectLayer;
 			const float mult = 2.f;
 			if (kb->getShiftKeyPressed()) {
 				layer->setPositionX(layer->getPositionX() - y * mult);
