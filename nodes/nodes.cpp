69c732e8fcce4de2ecc8343151e5cd0958b88205
matcool <26722564+matcool@users.noreply.github.com>
2026-04-27 10:31:32 -0300

    use geode utils here

--- nodes/nodes.cpp
+++ nodes/nodes.cpp
@@ -1,6 +1,4 @@
 #include "nodes.hpp"
-#include <array>
-#include <charconv>
 #include <fmt/core.h>
 
 using namespace cocos2d;
@@ -50,11 +48,7 @@ void NumberInputNode::set_value(int value) {
 }
 
 int NumberInputNode::get_value() {
-	try {
-		return std::stoi(input_node->getString());
-	} catch (const std::invalid_argument&) {
-		return -1;
-	}
+	return geode::utils::numFromString<int>(input_node->getString()).unwrapOr(-1);
 }
 
 
@@ -69,46 +63,11 @@ void FloatInputNode::textChanged(CCTextInputNode*) {
 }
 
 void FloatInputNode::set_value(float value) {
-	// TODO: not hardcode this
-	std::array<char, 10> arr;
-#ifndef __cpp_lib_to_chars
-	// macos std::to_chars does not support floats at all, awesome
-	const auto str = fmt::format("{}", value);
-	if (str.size() >= 10) {
-		input_node->setString("1");
-	} else {
-		input_node->setString(str);
-	}
-#else
-	const auto result = std::to_chars(arr.data(), arr.data() + arr.size(), value, std::chars_format::fixed);
-	if (result.ec == std::errc::value_too_large) {
-		// TODO: have this be adjustable
-		input_node->setString("1");
-	} else {
-		*result.ptr = 0;
-		input_node->setString(arr.data());
-	}
-#endif
+	input_node->setString(fmt::format("{}", value));
 }
 
 std::optional<float> parse_float(const std::string_view str) {
-#ifndef __cpp_lib_to_chars
-	// amazing i know, do this to avoid exceptions
-	std::stringstream stream;
-	stream << str;
-	stream.imbue(std::locale("C"));
-	float value;
-	if (!(stream >> value)) {
-		return {};
-	}
-	return value;
-#else
-	float value = 0.f;
-	const auto result = std::from_chars(str.data(), str.data() + str.size(), value);
-	if (result.ec == std::errc::invalid_argument)
-		return {};
-	return value;
-#endif
+	return geode::utils::numFromString<float>(str).ok();
 }
 
 std::optional<float> FloatInputNode::get_value() {
